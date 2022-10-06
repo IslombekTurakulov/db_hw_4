6bf7a5fae659e6ac55973c96b4265c8b4f8eb0f1
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-10-06 21:51:04 +0000

    changes

--- docker/db/scripts/station-hw3-1.sql
+++ docker/db/scripts/station-hw3-1.sql
@@ -1,23 +1,22 @@
-CREATE TABLE "public.Cities" (
+CREATE SCHEMA IF NOT EXISTS station;
+
+CREATE TABLE station.Cities (
     "region"    varchar NOT NULL,
     "city_name" varchar NOT NULL,
     PRIMARY KEY ("region", "city_name")
-) WITH (
-  OIDS=FALSE
-);
+)
 
-
-CREATE TABLE "public.Stations" (
+CREATE TABLE station.Stations (
     "name"  TEXT NOT NULL,
     "#tracks"    integer NOT NULL,
     "city_name"   varchar NOT NULL,
     "city_region" varchar NOT NULL,
     CONSTRAINT "Station_pk" PRIMARY KEY ("name"),
-    FOREIGN KEY ("city_region", "city_name") REFERENCES "public.Cities
+    FOREIGN KEY ("city_region", "city_name") REFERENCES station.Cities
 );
 
 
-CREATE TABLE "public.Trains" (
+CREATE TABLE station.Trains (
   "trainNR" SERIAL NOT NULL,
   "length" integer NOT NULL,
   "start_station_id" varchar NOT NULL,
@@ -26,7 +25,7 @@ CREATE TABLE "public.Trains" (
 );
 
 
-CREATE TABLE "public.Connected" (
+CREATE TABLE station.Connected (
   "trainNR" integer NOT NULL,
   "from_station_id" varchar NOT NULL,
   "to_station_id" varchar NOT NULL,
@@ -35,12 +34,12 @@ CREATE TABLE "public.Connected" (
 );
 
 
-ALTER TABLE "public.Trains" ADD CONSTRAINT "Trains_fk0" FOREIGN KEY ("start_station_id") REFERENCES "public.Stations"("name");
+ALTER TABLE station.Trains ADD CONSTRAINT "Trains_fk0" FOREIGN KEY ("start_station_id") REFERENCES station.Stations("name");
 
-ALTER TABLE "public.Trains" ADD CONSTRAINT "Trains_fk1"  FOREIGN KEY ("end_station_id") REFERENCES "public.Stations"("name");
+ALTER TABLE station.Trains ADD CONSTRAINT "Trains_fk1"  FOREIGN KEY ("end_station_id") REFERENCES station.Stations("name");
 
-ALTER TABLE "public.Connected" ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("trainNR") REFERENCES "public.Trains"("trainNR");
+ALTER TABLE station.Connected ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("trainNR") REFERENCES station.Trains("trainNR");
 
-ALTER TABLE "public.Connected" ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("from_station_id") REFERENCES "public.Stations"("name");
+ALTER TABLE station.Connected ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("from_station_id") REFERENCES station.Stations("name");
 
-ALTER TABLE "public.Connected" ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("to_station_id") REFERENCES "public.Stations"("name");
+ALTER TABLE station.Connected ADD CONSTRAINT "Connected_fk0"  FOREIGN KEY ("to_station_id") REFERENCES station.Stations("name");
