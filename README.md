e8169fa890df88c03b66725dd5db48cc6265335a
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-10-07 14:40:32 +0000

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Домашнее задание № 4
 ## Выполнил: Туракулов Исломбек Улугбекович
+
+
+#### Library
+
+------
+
+
+[](./tables/library-islam.png)
+
+
+#### Hospital
+
+------
+
+
+[](./tables/hospital-islam.png)
+
+
+#### Station
+
+------
+
+
+[](./tables/station-islam.png)
+
--- docker/db/scripts/library-hw2.sql
+++ docker/db/scripts/library-hw2.sql
@@ -1,53 +1,50 @@
 CREATE SCHEMA IF NOT EXISTS library;
 
 CREATE TABLE library.Category (
-	"categoryId" SERIAL PRIMARY KEY,
-	"categoryName" varchar NOT NULL,
-	"categoryParentId" integer NOT NULL
+  "categoryId" SERIAL PRIMARY KEY,
+  "categoryName" varchar NOT NULL,
+  "categoryParentId" integer NOT NULL
 );
 
 CREATE TABLE library.Book (
-	"bookYear" TIMESTAMP NOT NULL,
-	"bookName" varchar NOT NULL,
-	"bookAuthor" integer NOT NULL,
-	"bookPages" integer NOT NULL,
-	"ISBN" varchar(13) NOT NULL DEFAULT '1',
-	 PRIMARY KEY ("ISBN")
+  "bookYear" TIMESTAMP NOT NULL,
+  "bookName" varchar NOT NULL,
+  "bookAuthor" integer NOT NULL,
+  "bookPages" integer NOT NULL,
+  "ISBN" integer NOT NULL,
+   PRIMARY KEY ("ISBN")
 );
 
 
 CREATE TABLE library.Publisher (
-	"publisherId" SERIAL PRIMARY KEY,
-	"publisherName" varchar NOT NULL,
-	"publisherAddress" varchar NOT NULL
+  "publisherId" SERIAL PRIMARY KEY,
+  "publisherName" varchar NOT NULL,
+  "publisherAddress" varchar NOT NULL
 );
 
 
 CREATE TABLE library.BookCopy (
-	"bookCopyId" integer PRIMARY KEY,
-	"shelfPos" varchar NOT NULL
+  "bookCopyId" integer PRIMARY KEY,
+  "shelfPos" varchar NOT NULL
 );
 
 
 CREATE TABLE library.Rent (
-	"rentId" SERIAL PRIMARY KEY,
-	"rentReturnDate" TIMESTAMP NOT NULL,
-	"readerId" integer
+  "rentId" SERIAL PRIMARY KEY,
+  "rentReturnDate" TIMESTAMP NOT NULL,
+  "readerId" integer
 );
 
 
 CREATE TABLE library.Reader (
-	"id" SERIAL PRIMARY KEY,
-	"firstName" varchar NOT NULL,
-	"lastName" varchar,
-	"address" varchar NOT NULL,
-	"dateOfBirth" DATE
+  "id" SERIAL PRIMARY KEY,
+  "firstName" varchar NOT NULL,
+  "lastName" varchar,
+  "address" varchar NOT NULL,
+  "dateOfBirth" DATE
 );
 
-
-ALTER TABLE library.Category ADD FOREIGN KEY ("categoryId") REFERENCES library.Book("bookId");
-
-ALTER TABLE library.Category ADD FOREIGN KEY ("categoryName") REFERENCES library.Book("ISBN");
+ALTER TABLE library.Category ADD FOREIGN KEY ("categoryId") REFERENCES library.Book("ISBN");
 
 ALTER TABLE library.Category ADD FOREIGN KEY ("categoryParentId") REFERENCES library.Category("categoryId");
 
