1f2c5bb1301e26c95cadf29542581826ea071e61
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-10-07 14:12:16 +0000

    updated library

--- docker/db/scripts/library-hw2.sql
+++ docker/db/scripts/library-hw2.sql
@@ -3,15 +3,16 @@ CREATE SCHEMA IF NOT EXISTS library;
 CREATE TABLE library.Category (
 	"categoryId" SERIAL PRIMARY KEY,
 	"categoryName" varchar NOT NULL,
-	"categoryParentId" varchar NOT NULL
+	"categoryParentId" integer NOT NULL
 );
 
 CREATE TABLE library.Book (
 	"bookYear" TIMESTAMP NOT NULL,
 	"bookName" varchar NOT NULL,
-	"bookAuthor" varchar NOT NULL,
+	"bookAuthor" integer NOT NULL,
 	"bookPages" integer NOT NULL,
-	"ISBN" varchar(13) NOT NULL DEFAULT '1' PRIMARY KEY
+	"ISBN" varchar(13) NOT NULL DEFAULT '1',
+	 PRIMARY KEY ("ISBN")
 );
 
 
@@ -23,14 +24,15 @@ CREATE TABLE library.Publisher (
 
 
 CREATE TABLE library.BookCopy (
-	"bookCopyId" serial NOT NULL,
+	"bookCopyId" integer PRIMARY KEY,
 	"shelfPos" varchar NOT NULL
 );
 
 
 CREATE TABLE library.Rent (
 	"rentId" SERIAL PRIMARY KEY,
-	"rentReturnDate" TIMESTAMP NOT NULL
+	"rentReturnDate" TIMESTAMP NOT NULL,
+	"readerId" integer
 );
 
 
@@ -44,13 +46,14 @@ CREATE TABLE library.Reader (
 
 
 ALTER TABLE library.Category ADD FOREIGN KEY ("categoryId") REFERENCES library.Book("bookId");
+
 ALTER TABLE library.Category ADD FOREIGN KEY ("categoryName") REFERENCES library.Book("ISBN");
+
 ALTER TABLE library.Category ADD FOREIGN KEY ("categoryParentId") REFERENCES library.Category("categoryId");
 
+ALTER TABLE library.Publisher ADD FOREIGN KEY ("publisherId") REFERENCES library.Book("ISBN");
 
-ALTER TABLE library.Publisher ADD FOREIGN KEY ("publisherId") REFERENCES library.Book("bookId");
-
-ALTER TABLE library.BookCopy ADD FOREIGN KEY ("bookCopyId") REFERENCES library.Book("bookId");
+ALTER TABLE library.BookCopy ADD FOREIGN KEY ("bookCopyId") REFERENCES library.Book("ISBN");
 
 ALTER TABLE library.Rent ADD FOREIGN KEY ("rentId") REFERENCES library.BookCopy("bookCopyId");
 
